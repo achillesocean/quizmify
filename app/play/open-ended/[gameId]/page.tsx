ac757b21a64259f516f96884a0e5010329794053
Felmeta Dereje <0202sent@gmail.com>
2025-10-01 23:34:57 +0300

    open ended questions page

--- app/play/open-ended/[gameId]/page.tsx
+++ app/play/open-ended/[gameId]/page.tsx
@@ -1,9 +1,42 @@
+import OpenEnded from "@/components/OpenEnded";
+import { prisma } from "@/lib/db";
+import { getAuthSession } from "@/lib/nextauth";
+import { redirect } from "next/navigation";
 import React from "react";
 
-type Props = {};
+type Props = {
+  params: {
+    gameId: string;
+  };
+};
 
-const OpenEndedPage = (props: Props) => {
-  return <div>OpenEndedPage</div>;
+const OpenEndedPage = async ({ params: { gameId } }: Props) => {
+  // what is the ': {gameId}'
+  const session = await getAuthSession();
+  if (!session?.user) {
+    return redirect("/");
+  }
+
+  const game = await prisma.game.findUnique({
+    where: {
+      id: gameId,
+    },
+    include: {
+      questions: {
+        select: {
+          id: true, // explore the addition of this id
+          question: true,
+          answer: true, // needed to add this, explain!?
+          // options: true,
+        },
+      },
+    },
+  });
+  if (!game || game.gameType !== "open_ended") {
+    return redirect("/quiz");
+  }
+
+  return <OpenEnded game={game} />;
 };
 
 export default OpenEndedPage;
